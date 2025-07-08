b60ede5f13ba2e76892d417b88f4f919002740d4
VEIDD <separik3@gmail.com>
2025-07-08 17:06:24 +0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { CreateQuiz } from './components/CreateQuiz'
 import { StartQuiz } from './components/StartQuiz';
 import { Result } from './components/Result';
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/quiz-app">
     <Routes>
       <Route path="/" element={<Layout />}/>
       <Route path='/create-quiz' element={<CreateQuiz/>}/>
